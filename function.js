2f69b8845b785cd0bacb8b71644500bfb295ee68
nithin1245 <nithin.sirikonda1245@gmail.com>
2025-09-22 12:37:04 +0530

    function

--- function.js
+++ function.js
@@ -8,10 +8,27 @@
 // }
 
 // add(); 
-const add = () => {
-    var a = 67;
-    var b = 78;
-    document.write(a + b);
-    console.log(a + b);
-};
-add();
+// const add = () => {
+//     var a = 67;
+//     var b = 78;
+//     document.write(a + b);
+//     console.log(a + b);
+// };
+// add();
+//ES7
+// const add = (a, b) => console.log(a + b);
+// add(78, 90);
+console.log(b);
+b = 45;
+// to declare the variable to the top
+console.log(b);
+b = 45;
+
+let name = "sirikonda";
+
+function demo() {
+    let name = "srinu";
+    document.write("Hello, my name is ${name}");
+}
+document.write("Hello, my name is ${name}");
+demo();
