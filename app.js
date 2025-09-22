6e51d1504be1be1ea50b1d02f77a593d1492b435
nithin1245 <nithin.sirikonda1245@gmail.com>
2025-09-22 11:02:25 +0530

    datatype

--- app.js
+++ app.js
@@ -9,4 +9,13 @@ let b = 68;
 const c = 78;
 
 console.log("value b is ${b}");
+console.log("value c is ${c}");
 console.log(a);
+
+//comment -
+//single line comment
+//data types
+//1.primitive 2.non-primitive
+//primive - number, string, boolean, null, undefined, symbol
+//non-primitive - object, array
+
---
+++ datatype.js
@@ -0,0 +1,10 @@
+a = 7897;
+console.log(typeof a);
+name = "sirikonda";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
+data = null;
+console.log(typeof data);
+d = undefined;
+console.log(typeof d);
