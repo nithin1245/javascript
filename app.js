710405a99058a8c57296d3797dbdcb8f594f8760
nithin1245 <nithin.sirikonda1245@gmail.com>
2025-09-22 10:28:19 +0530

    variable

---
+++ app.js
@@ -0,0 +1,12 @@
+//types of variables
+//var, let, const
+//var - global variable
+//left const - local variable
+var a = 67;
+var a = 90;
+let b = 68;
+
+const c = 78;
+
+console.log("value b is ${b}");
+console.log(a);
