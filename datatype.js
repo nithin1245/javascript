6f68d584cb5d532c2b298160daaba892adcc6fb0
nithin1245 <nithin.sirikonda1245@gmail.com>
2025-09-22 11:59:01 +0530

    function

--- datatype.js
+++ datatype.js
@@ -8,3 +8,14 @@ data = null;
 console.log(typeof data);
 d = undefined;
 console.log(typeof d);
+
+//non-primitive
+Array = [56, "john", true, null];
+console.log(typeof Array);
+obj = {
+    name: "sirikonda",
+    age: 23,
+    city: "hyd",
+};
+console.log(typeof data);
+
---
+++ function.js
@@ -0,0 +1,17 @@
+//function
+//code repository
+// function add(){
+//     var a = 67;
+//     var b = 78;
+//     document.write(a + b);
+//     console.log(a + b);
+// }
+
+// add(); 
+const add = () => {
+    var a = 67;
+    var b = 78;
+    document.write(a + b);
+    console.log(a + b);
+};
+add();
